7a693963eedae7998b0e7350d0e1c3f0d24d7c08
Berk Demirci <101094624+enesbrk@users.noreply.github.com>
2026-01-03 15:50:41 +0300

    Gemini Model Update

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ Ana dilin İngilizce'dir. Ancak kullanıcı Türkçe sorarsa akıcı bir TÜrkç
 
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-001",
+    model_name="gemini-2.0-flash",
     system_instruction=SYSTEM_INSTRUCTION
 )
 
